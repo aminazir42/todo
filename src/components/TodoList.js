5187e0a6a390df80036b2464be0f344a71498fa9
Amina Nazir <aminazir23@gmail.com>
2024-05-17 13:03:58 +0500

    comit

---
+++ src/components/TodoList.js
@@ -0,0 +1,54 @@
+import React, { useState } from 'react';
+import { connect } from 'react-redux';
+import { addTodo, removeTodo } from '../actions/todoActions';
+import { Box, Button, Input, List, ListItem, IconButton, Heading } from '@chakra-ui/react';
+import { DeleteIcon } from '@chakra-ui/icons';
+
+const TodoList = ({ todos, addTodo, removeTodo }) => {
+  const [todo, setTodo] = useState('');
+
+  const handleAddTodo = () => {
+    if (todo.trim() !== '') {
+      addTodo(todo);
+      setTodo('');
+    }
+  };
+
+  return (
+    <Box p="5" maxW="500px" mx="auto">
+      <Heading mb="4">Todo List</Heading>
+      <Box display="flex" mb="4">
+        <Input
+          value={todo}
+          onChange={(e) => setTodo(e.target.value)}
+          placeholder="Add a new task"
+          mr="2"
+        />
+        <Button onClick={handleAddTodo} colorScheme="teal">Add</Button>
+      </Box>
+      <List spacing={3}>
+        {todos.map((todo, index) => (
+          <ListItem key={index} display="flex" justifyContent="space-between">
+            {todo}
+            <IconButton
+              icon={<DeleteIcon />}
+              colorScheme="red"
+              onClick={() => removeTodo(index)}
+            />
+          </ListItem>
+        ))}
+      </List>
+    </Box>
+  );
+};
+
+const mapStateToProps = (state) => ({
+  todos: state.todos,
+});
+
+const mapDispatchToProps = {
+  addTodo,
+  removeTodo,
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(TodoList);
