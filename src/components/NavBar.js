769a57e27fe09e526db83c4693ac652dcd61ba7f
Amina Nazir <aminazir23@gmail.com>
2024-05-18 16:45:26 +0500

    kkk

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -5,14 +5,9 @@ const NavBar = () => {
   return (
     <Box bg="teal.500" px={4} py={3}>
       <Flex h={16} alignItems="center" justifyContent="space-between">
-        <Text fontSize="xl" fontWeight="bold" color="white">Todo App</Text>
-        <HStack spacing={8} alignItems="center">
-          <Link href="#" color="white">Home</Link>
-          <Link href="#" color="white">About</Link>
-          <Link href="#" color="white">Contact</Link>
-        </HStack>
+        <Text fontSize="xl" fontWeight="bold" color="white" fontFamily="Lucida Handwriting">Todoo</Text>
       </Flex>
     </Box>
-  );
+  )
 };
 export default NavBar;
